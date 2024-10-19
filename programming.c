d397d4c006e119427e6630d517c86fdebbf5bb17
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-20 03:02:39 +0600

    Min Max of an Array

--- programming.c
+++ programming.c
@@ -117,7 +117,6 @@
 //         a[i] += 100;
 //     }
 
-
 //     for (int i = 0; i < 5; i++)
 //     {
 
@@ -142,7 +141,6 @@
 
 //         a[i] *= 100;
 //     }
-
 
 //     for (int i = 0; i < 5; i++)
 //     {
@@ -170,22 +168,156 @@
 //     }
 
 // }
+// #include <stdio.h>
+// int main()
+// {
+
+//     char a[5];
+
+//     for (int i = 0; i < 5; i++)
+//     {
+
+//         scanf("%c", &a[i]);
+//     }
+
+//     for (int i = 0; i < 5; i++)
+//     {
+
+//         printf("%d ", &a[i]);
+//     }
+
+// }
+// #include <stdio.h>
+// int main()
+// {
+
+//     int n;
+
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         scanf("%d", &a[i]);
+//     }
+
+//     int sum = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         sum += a[i];
+//     }
+
+//     printf("%d\n", sum);
+
+//     return 0;
+// }
+// #include <stdio.h>
+// int main()
+// {
+
+//     int n;
+
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         scanf("%d", &a[i]);
+//     }
+
+//     int sum = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         sum += a[i];
+//     }
+
+//     printf("%d\n", sum);
+
+//     double avg = (double) sum / n;
+
+//     printf("%lf\n", avg);
+
+//     return 0;
+// }
+// #include <stdio.h>
+// #include <limits.h>
+
+// int main()
+// {
+
+//     int n;
+
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         scanf("%d", &a[i]);
+//     }
+
+//     int min = INT_MAX, max = INT_MIN;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         if(a[i] < min){
+//             min = a[i];
+//         }
+
+//         if(a[i] > max){
+//             max = a[i];
+//         }
+//     }
+
+//     printf("Minimum: %d, Maximum: %d\n", min, max);
+
+//     return 0;
+// }
 #include <stdio.h>
+#include <limits.h>
+
 int main()
 {
 
-    char a[5];
+    int n;
 
-    for (int i = 0; i < 5; i++)
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
     {
 
-        scanf("%c", &a[i]);
+        scanf("%d", &a[i]);
     }
 
-    for (int i = 0; i < 5; i++)
+    int min = a[0], max = a[0];
+
+    for (int i = 0; i < n; i++)
     {
 
-        printf("%d ", &a[i]);
+        if(a[i] < min){
+            min = a[i];
+        }
+
+        if(a[i] > max){
+            max = a[i];
+        }
+
+        printf("Min: %d, Max: %d\n", min, max);
     }
 
+    printf("Minimum: %d, Maximum: %d\n", min, max);
+
+    return 0;
 }
